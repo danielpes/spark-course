48c4cfdc1b96e5dd18a5f77be749b0f55997348c
Daniel de Paula e Silva <danielpes91@gmail.com>
2018-02-05 22:08:53 +0100

    Fix slides for class 1 and 2

--- js/init-reveal.js
+++ js/init-reveal.js
@@ -13,6 +13,7 @@ Reveal.initialize({
       { src: '../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
       { src: '../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
       { src: '../reveal.js/plugin/zoom-js/zoom.js', async: true },
-      { src: '../reveal.js/plugin/notes/notes.js', async: true }
+      { src: '../reveal.js/plugin/notes/notes.js', async: true },
+      { src: '../reveal.js/plugin/math/math.js', async: true }
   ]
 });
