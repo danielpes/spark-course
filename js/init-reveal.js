357ff38f8ff412335aad03c7fe8654b4bba9e43d
Daniel de Paula e Silva <danielpes91@gmail.com>
2018-02-05 22:31:23 +0100

    Fix slides for class 3

--- js/init-reveal.js
+++ js/init-reveal.js
@@ -11,7 +11,7 @@ Reveal.initialize({
       { src: '../reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
       { src: '../reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
       { src: '../reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-      { src: '../reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+      { src: '../reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
       { src: '../reveal.js/plugin/zoom-js/zoom.js', async: true },
       { src: '../reveal.js/plugin/notes/notes.js', async: true },
       { src: '../reveal.js/plugin/math/math.js', async: true }
